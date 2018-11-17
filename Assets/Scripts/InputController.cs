3579f232628c8bdb7469769670dd059fe02fe9d7
Aymeric Gleye <gleye.a@gmail.com>
2018-11-17 10:04:29 +0100

    Beginning of the project
    Bug correction and start InputController

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class InputController : MonoBehaviour {
 
-	// Use this for initialization
+    // Use this for initialization
+    private Vector2 axis;
+
+    void Move(Vector2 nextPosition)
+    {
+
+    }
 	void Start () {
-		
+        axis = new Vector2(0, 0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        axis.x = Input.GetAxis("Horizontal");
+        axis.y = Input.GetAxis("Vertical");
+        Move(axis);
+        if (Input.GetAxis("Action") > 0)
+        {
+
+        }
 	}
 }
