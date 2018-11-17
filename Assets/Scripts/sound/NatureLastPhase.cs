ff72542538c1bfeb2e30fce47a092086a2726369
Raminka <ramina.suleimanova@telecom-sudparis.eu>
2018-11-17 20:33:19 +0100

    sounds begining

---
+++ Assets/Scripts/sound/NatureLastPhase.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NatureLastPhase : MonoBehaviour
+{
+
+    //public AudioSource audioSource;
+    AudioSource audioData;
+
+    TimeStream timeStream;
+    int date;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        timeStream = TimeStream.Instance;
+        date = timeStream.getTime();
+
+        audioData = GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (date >= 3)
+        {
+            audioData.Play();
+        }
+    }
+}
