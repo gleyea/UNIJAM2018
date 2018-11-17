dbc26d8755b996f4d0175b42828f1fc6bf0ac8b8
MaisieHelynn <sylia.mehenni@ensiie.fr>
2018-11-17 16:23:39 +0100

    Menu

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Script de l'écran titre
+/// </summary>
+public class MenuScript : MonoBehaviour
+{
+    public void PlayScene()
+    {
+        // Sur le clic, on démarre le premier niveau
+        // "MainScene" est le nom de la première scène que nous avons créés.
+        SceneManager.LoadScene("MainScene");
+
+    }
+
+    public void QuitScene()
+    {
+        Application.Quit();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+    }
+}
