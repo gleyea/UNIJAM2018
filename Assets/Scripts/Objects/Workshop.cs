d6be33f02f383dfbf193ef4bd3684fa9cf63065a
Raminka <ramina.suleimanova@telecom-sudparis.eu>
2018-11-17 16:47:07 +0100

    Workshop sprite addition

--- Assets/Scripts/Objects/Workshop.cs
+++ Assets/Scripts/Objects/Workshop.cs
@@ -10,6 +10,26 @@ public class Workshop : ObjectManager
 
     int nbWood;
 
+
+
+    [SerializeField]
+    Sprite atelier0;
+
+    [SerializeField]
+    Sprite atelier1;
+
+    [SerializeField]
+    Sprite atelier2;
+
+    [SerializeField]
+    Sprite atelier3;
+
+    [SerializeField]
+    Sprite atelier4;
+
+    [SerializeField]
+    Sprite atelier5;
+
     private SpriteRenderer spriteRenderer;
 
     // Use this for initialization
@@ -19,22 +39,50 @@ public class Workshop : ObjectManager
         spriteRenderer = GetComponent<SpriteRenderer>();
         if (spriteRenderer.sprite == null)
             spriteRenderer.sprite = workshopSprite;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer.sprite == null)
+            spriteRenderer.sprite = atelier0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (nbWood == 0 & spriteRenderer.sprite != atelier0)
+        {
+            spriteRenderer.sprite = atelier0;
+        }
+        else if (nbWood == 1 & spriteRenderer.sprite != atelier1)
+        {
+            spriteRenderer.sprite = atelier1;
+        }
+        else if (nbWood == 2 & spriteRenderer.sprite != atelier2)
+        {
+            spriteRenderer.sprite = atelier2;
+        }
+        else if (nbWood == 3 & spriteRenderer.sprite != atelier3)
+        {
+            spriteRenderer.sprite = atelier3;
+        }
+        else if (nbWood == 4 & spriteRenderer.sprite != atelier4)
+        {
+            spriteRenderer.sprite = atelier4;
+        }
+        else if (nbWood == 5 & spriteRenderer.sprite != atelier5)
+        {
+            spriteRenderer.sprite = atelier5;
+        }
     }
 
     public override void activate()
     {
-        if (nbWood < 6 & GetComponent<Player>().HasWoodLog)
+        if (nbWood < 5 & GetComponent<Player>().HasWoodLog)
         {
             nbWood++;
             GetComponent<Player>().HasWoodLog = false;
         }
-        if (nbWood == 6)
+        if (nbWood == 5)
         {
+            nbWood = 0;
             /*
              * 
              * 
