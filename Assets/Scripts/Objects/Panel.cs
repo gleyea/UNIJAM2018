3ad36237c9c21f243ffb5a92ed227eab5f559e12
Aymeric Gleye <gleye.a@gmail.com>
2018-11-18 01:55:54 +0100

    Refactor scene

---
+++ Assets/Scripts/Objects/Panel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Panel : ObjectManager {
+
+    TimeStream timeStream;
+
+    // Use this for initialization
+    void Start () {
+        timeStream = TimeStream.Instance;
+    }
+
+    public override void activate()
+    {
+
+     }
+    // Update is called once per frame
+    void Update () {
+		if (player)
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+        }
+        else if (!player)
+        {
+            transform.GetChild(0).gameObject.SetActive(false);
+        }
+    }
+}
